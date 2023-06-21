a5fe40634c6f69aaa4e31dfb2d992c54f19a8b44
Oleg <tradecenter1771@gmail.com>
2023-06-21 10:50:53 +0300

    Added invitation

---
+++ Ex_3.py
@@ -0,0 +1,9 @@
+guests = ['Billy', 'Andy', 'Leo', 'Luca']
+popped_guest = guests.pop(-2)
+guests.insert(-1, 'Luis')
+print(f'{guests[0]}, I want to invite you on my birthday party')
+print(f'{guests[1]}, I want to invite you on my birthday party')
+print(f'{guests[2]}, I want to invite you on my birthday party')
+print(f'{guests[3]}, I want to invite you on my birthday party')
+
+print(f'{popped_guest} cant come on party')
