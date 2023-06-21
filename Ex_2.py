c90d67df1e7807d9d35eae395ec1910b5705cd53
Oleg <tradecenter1771@gmail.com>
2023-06-21 10:35:30 +0300

    Added invitation

---
+++ Ex_2.py
@@ -0,0 +1,5 @@
+guests = ['Billy', 'Andy', 'Leo', 'Luca']
+print(f'{guests[0]}, I want to invite you on my birthday party')
+print(f'{guests[1]}, I want to invite you on my birthday party')
+print(f'{guests[2]}, I want to invite you on my birthday party')
+print(f'{guests[3]}, I want to invite you on my birthday party')
