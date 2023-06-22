6e4cce21f071b394cbe1afa5b4478030e5edef99
Oleg <tradecenter1771@gmail.com>
2023-06-22 15:19:41 +0300

    Added

---
+++ If/Ex_2.py
@@ -0,0 +1,12 @@
+favorite_fruits = ['melon', 'cherry', 'apple']
+for fruit in favorite_fruits:
+    if fruit == 'lemon':
+        print('You really like bananas')
+    if fruit == 'pineapple':
+        print('You really like bananas')
+    if fruit == 'melon':
+        print('You really like bananas')
+    if fruit == 'cherry':
+        print('You really like bananas')
+    if fruit == 'apple':
+        print('You really like bananas')
