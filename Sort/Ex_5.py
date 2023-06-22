49aca154aebef9ad7e4a588acbbc7b719e9d8532
Oleg <tradecenter1771@gmail.com>
2023-06-22 14:14:01 +0300

    Added sort

---
+++ Sort/Ex_5.py
@@ -0,0 +1,4 @@
+countries = ['Spain', 'Italy', 'Greece', 'USA', 'Thailand', 'England']
+print(f'The first three items in the list are: {countries[0:3]}')
+print(f'The items from the middle of the list are: {countries[1:4]}')
+print(f'The last three items in the list are: {countries[-3:]}')
