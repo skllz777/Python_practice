666590d1b51a1b92f798c8bc4e41d57085c905a6
Oleg <tradecenter1771@gmail.com>
2023-06-22 15:13:48 +0300

    Added

---
+++ If/Ex_1.py
@@ -0,0 +1,15 @@
+age = 78
+if age < 2:
+    print('Младенец')
+elif 2 <= age < 4:
+    print('Малыш')
+elif 4 <= age < 13:
+    print("Ребенок")
+elif 13 <= age < 20:
+    print('Подросток')
+elif 20 <= age < 65:
+    print('Взрослый')
+else:
+    print("Пожилой")
+
+
