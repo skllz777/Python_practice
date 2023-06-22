b03cef604ef78d66eed724e5f3bab0116921ff9d
Oleg <tradecenter1771@gmail.com>
2023-06-22 14:27:47 +0300

    Added sort

---
+++ Sort/Ex_7.py
@@ -0,0 +1,6 @@
+menu = ("salad", "beef", "chicken", "icecream", "muffin")
+for food in menu:
+    print(food)
+menu = ("salad", "beef", "chicken", "potato", "candy")
+for food in menu:
+    print(food)
