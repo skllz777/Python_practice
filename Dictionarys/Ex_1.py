319530e993284adfa509192c476a30d565ee2197
Oleg <tradecenter1771@gmail.com>
2023-06-26 12:08:24 +0300

    Added

--- Dictionarys/Ex_1.py
+++ Dictionarys/Ex_1.py
@@ -1,10 +1,13 @@
-man = {
-    'first_name': 'Alek',
-    'last_name': 'Bolduin',
-    'age': 52,
-    'city': 'Ney York',
+fav_digits = {
+    'Alek': 8,
+    'Bob': 5,
+    'Fil': 79,
+    'Den': 10,
+    'Mike': 77,
 }
-print(man['first_name'])
-print(man['last_name'])
-print(man['age'])
-print(man['city'])
+
+print(f"Aleks favorite language is {fav_digits['Alek']}")
+print(f"Bob favorite language is {fav_digits['Bob']}")
+print(f"Fils favorite language is {fav_digits['Fil']}")
+print(f"Dens favorite language is {fav_digits['Den']}")
+print(f"Miles favorite language is {fav_digits['Mike']}")
