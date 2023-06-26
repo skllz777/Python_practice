eebf3aebd14eb35a3bdc9b8abe2216b2effc4353
Oleg <tradecenter1771@gmail.com>
2023-06-26 14:54:24 +0300

    Added

---
+++ Dictionarys/Ex_4.py
@@ -0,0 +1,23 @@
+man_1 = {
+    'first_name': 'cristiano',
+    'last_name': 'ronaldo',
+    'age': 39,
+    'country': 'portugal',
+}
+man_2 = {
+    'first_name': 'zinadine',
+    'last_name': 'zidane',
+    'age': 49,
+    'country': 'france',
+}
+man_3 = {
+    'first_name': 'carlo',
+    'last_name': 'ancelotti',
+    'age': 59,
+    'country': 'italy',
+}
+people = [man_1, man_2, man_3]
+for peoples in people:
+    print(peoples)
+
+
