0fa77889f00f5b5b10ab5ff8af76ee0e5d9bb269
Oleg <tradecenter1771@gmail.com>
2023-06-26 11:59:49 +0300

    Added

---
+++ Dictionarys/Ex_1.py
@@ -0,0 +1,10 @@
+man = {
+    'first_name': 'Alek',
+    'last_name': 'Bolduin',
+    'age': 52,
+    'city': 'Ney York',
+}
+print(man['first_name'])
+print(man['last_name'])
+print(man['age'])
+print(man['city'])
