09b2cfd79b1762a7cf550a81fc90c082da0ea2f5
Oleg <tradecenter1771@gmail.com>
2023-06-26 15:47:44 +0300

    Added

---
+++ Dictionarys/Ex_5.py
@@ -0,0 +1,8 @@
+favorite_places = {
+    'beach': 'coral',
+    'city': 'sydney',
+    'sea': 'red sea',
+}
+print(f"Billys favorite place is {favorite_places['beach']}")
+print(f"Anna likes {favorite_places['city']} and {favorite_places['sea']}")
+print(f"Jonnhy likes to stop in {favorite_places['city']}")
