77199b46fc43e1c7c028c3f9fe34d5135acbc25d
Oleg <tradecenter1771@gmail.com>
2023-06-26 12:39:13 +0300

    Added

---
+++ Dictionarys/Ex_2.py
@@ -0,0 +1,7 @@
+rivers = {
+    'nile': 'egypt',
+    'volga': 'russia',
+    'dnepr': 'ukraine',
+}
+for key, value in rivers.items():
+    print(f"The {key.title()} is runs through {value.title()}")
