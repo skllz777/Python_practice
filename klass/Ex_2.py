ae2b4e30148d05ecdd9d9f46b72a43e1a2412d1c
Oleg <tradecenter1771@gmail.com>
2023-07-11 14:12:30 +0300

    Added

--- klass/Ex_2.py
+++ klass/Ex_2.py
@@ -1,28 +1,33 @@
 class User():
-    def __init__(self, firs_name, last_name, age, city):
+    def __init__(self, firs_name, last_name, age, city, login_attempts):
         self.name = firs_name
         self.l_name = last_name
         self.age = age
         self.city = city
+        self.login = login_attempts
     def describe_user(self):
         print(f'Name of user is: {self.name}')
         print(f'Last name of user is: {self.l_name}')
         print(f'Age of user is: {self.age}')
         print(f'User living in: {self.city}')
+        print(f'Loging attempts is {self.login}')
     def greet_user(self):
         print(f'Hello {self.name} {self.l_name}!\n')
+    def increment_login_atempts(self, atempts):
+        self.login += atempts
+    def reset_login_atempts(self):
+        self.login = 0
 
-user_1 = User('John', 'Jones', 31, 'Ney-York')
-user_2 = User('Daniel', 'Cormier', 44, 'Florida')
-user_3 = User('Khabib', 'Nurmagomedov', 34, 'Makhachkala')
 
+user_1 = User('John', 'Jones', 31, 'Ney-York', 0)
+
+
+user_1.increment_login_atempts(1)
+user_1.increment_login_atempts(1)
 user_1.describe_user()
 user_1.greet_user()
+user_1.reset_login_atempts()
+user_1.describe_user()
 
-user_2.describe_user()
-user_2.greet_user()
-
-user_3.describe_user()
-user_3.greet_user()
 
 
