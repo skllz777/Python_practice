30a9559dca47fb5cb36a1be0ebce6db2a633272d
Oleg <tradecenter1771@gmail.com>
2023-07-13 11:08:00 +0300

    Added

---
+++ klass/Ex_7.py
@@ -0,0 +1,56 @@
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f'{self.year} {self.make} {self.model}'
+
+    def read_odometer(self):
+        print(f'This car has {self.odometer_reading} miles on it')
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You cant roll back an odometer")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def describe_battery(self):
+        print(f'This car has a {self.battery_size}-kWh battery.')
+
+    def fill_gas_tank(self):
+        print("This car has no a gas tank!")
+
+
+class Battery():
+    def __init__(self, battery_size=75):
+        self.battery_size = battery_size
+
+    def get_range(self):
+        global range
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge")
+
+    def upgrade_battery(self):
+        if self.battery_size != 100:
+            self.battery_size = 100
+
+
+my_tesla = ElectricCar('Tesla', '2', 2023)
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
